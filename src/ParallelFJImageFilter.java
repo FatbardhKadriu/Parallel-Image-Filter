c1bd404144d2d23f87bc59c5c3419884453b7823
Fatbardh <fatbardh.kadriu@gmail.com>
2021-12-13 20:51:09 +0100

    implement fork join model class

--- src/ParallelFJImageFilter.java
+++ src/ParallelFJImageFilter.java
@@ -1,52 +1,66 @@
-public class ParallelFJImageFilter {
+import java.util.concurrent.RecursiveAction;
+
+public class ParallelFJImageFilter extends RecursiveAction {
     private int[] src;
     private int[] dst;
     private int width;
-    private int height;
+    private int start;
+    private int end;
 
-    private final int NRSTEPS = 100;
+    private final int THRESHOLD = 5;
 
-    public ParallelFJImageFilter(int[] src, int[] dst, int w, int h) {
+    public ParallelFJImageFilter(int[] src, int[] dst, int width, int start, int end) {
         this.src = src;
         this.dst = dst;
-        width = w;
-        height = h;
+        this.width = width;
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    protected void compute() {
+        if (start - end < THRESHOLD) {
+            apply();
+            return;
+        }
+
+        double preciseMiddle = (start + end) / 2.0;
+        int middle = (int) Math.round(preciseMiddle / 10.0) * 10;
+
+        invokeAll(new ParallelFJImageFilter(src, dst, width, start, middle),
+                new ParallelFJImageFilter(src, dst, width, middle, end));
     }
 
     public void apply() {
         int index, pixel;
-        for (int steps = 0; steps < NRSTEPS; steps++) {
-            for (int i = 1; i < height - 1; i++) {
-                for (int j = 1; j < width - 1; j++) {
-                    float rt = 0, gt = 0, bt = 0;
-                    for (int k = i - 1; k <= i + 1; k++) {
-                        index = k * width + j - 1;
-                        pixel = src[index];
-                        rt += (float) ((pixel & 0x00ff0000) >> 16);
-                        gt += (float) ((pixel & 0x0000ff00) >> 8);
-                        bt += (float) ((pixel & 0x000000ff));
+        for (int i = start; i < end; i++) {
+            for (int j = 1; j < width - 1; j++) {
+                float rt = 0, gt = 0, bt = 0;
+                for (int k = i - 1; k <= i + 1; k++) {
+                    index = k * width + j - 1;
+                    pixel = src[index];
+                    rt += (float) ((pixel & 0x00ff0000) >> 16);
+                    gt += (float) ((pixel & 0x0000ff00) >> 8);
+                    bt += (float) ((pixel & 0x000000ff));
 
-                        index = k * width + j;
-                        pixel = src[index];
-                        rt += (float) ((pixel & 0x00ff0000) >> 16);
-                        gt += (float) ((pixel & 0x0000ff00) >> 8);
-                        bt += (float) ((pixel & 0x000000ff));
+                    index = k * width + j;
+                    pixel = src[index];
+                    rt += (float) ((pixel & 0x00ff0000) >> 16);
+                    gt += (float) ((pixel & 0x0000ff00) >> 8);
+                    bt += (float) ((pixel & 0x000000ff));
 
-                        index = k * width + j + 1;
-                        pixel = src[index];
-                        rt += (float) ((pixel & 0x00ff0000) >> 16);
-                        gt += (float) ((pixel & 0x0000ff00) >> 8);
-                        bt += (float) ((pixel & 0x000000ff));
-                    }
-                    // Re-assemble destination pixel.
-                    index = i * width + j;
-                    int dpixel = (0xff000000) | (((int) rt / 9) << 16) | (((int) gt / 9) << 8) | (((int) bt / 9));
-                    dst[index] = dpixel;
+                    index = k * width + j + 1;
+                    pixel = src[index];
+                    rt += (float) ((pixel & 0x00ff0000) >> 16);
+                    gt += (float) ((pixel & 0x0000ff00) >> 8);
+                    bt += (float) ((pixel & 0x000000ff));
                 }
+                // Re-assemble destination pixel.
+                index = i * width + j;
+                int dpixel = (0xff000000) | (((int) rt / 9) << 16) | (((int) gt / 9) << 8) | (((int) bt / 9));
+                dst[index] = dpixel;
             }
-            // swap references
-            int[] help; help = src; src = dst; dst = help;
         }
     }
-
 }
+
--- src/TestImageFilter.java
+++ src/TestImageFilter.java
@@ -1,14 +1,19 @@
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.ForkJoinPool;
 import javax.imageio.IIOException;
 
 import javax.imageio.ImageIO;
 
 public class TestImageFilter {
+	private static final int NRSTEPS = 100;
+	private static String srcFileName = null;
+	private static long timeOfSequentialExecution;
+	private static long timeOfParallelExecution;
 
 	public static void main(String[] args) throws Exception {
 		BufferedImage image = null;
-		String srcFileName = null;
 		String srcPath = null;
 		try {
 			srcFileName = args[0];
@@ -29,21 +34,35 @@ public class TestImageFilter {
 
 		int w = image.getWidth();
 		int h = image.getHeight();
-		System.out.println("Image size is " + w + "x" + h);
-		System.out.println();
-	
+		System.out.println("Image size is " + w + "x" + h + "\n");
+
 		int[] src = image.getRGB(0, 0, w, h, null, 0, w);
+
 		int[] dst = new int[src.length];
 
+//		execSequentialFilter(src, dst, w, h);
+
+		System.out.println("\nAvailable processors: " + Runtime.getRuntime().availableProcessors() / 2);
+
+		int[] parallelSource = image.getRGB(0, 0, w, h, null, 0 , w);
+		int[] parallelDst = new int[parallelSource.length];
+
+//		execParallelFilter(parallelSource, parallelDst, w, h, 2);
+		ComparingImages.diff("src/SequentialFilteredImages/FilteredIMAGE1.JPG",
+				"src/ParallelFilteredImages/FilteredIMAGE1.jpg");
+	}
+
+	private static void execSequentialFilter(int[] src, int[] dst, final int w, final int h) throws IOException {
 		System.out.println("Starting sequential image filter.");
 
 		long startTime = System.currentTimeMillis();
-		ImageFilter filter0 = new ImageFilter(src, dst, w, h);
-		filter0.apply();
+		ImageFilter filter = new ImageFilter(src, dst, w, h);
+		filter.apply();
 		long endTime = System.currentTimeMillis();
 
-		long tSequential = endTime - startTime; 
+		long tSequential = endTime - startTime;
 		System.out.println("Sequential image filter took " + tSequential + " milliseconds.");
+		timeOfSequentialExecution = tSequential;
 
 		BufferedImage dstImage = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
 		dstImage.setRGB(0, 0, w, h, dst, 0, w);
@@ -53,6 +72,38 @@ public class TestImageFilter {
 		ImageIO.write(dstImage, "jpg", dstFile);
 
 		System.out.println("Output image: " + dstName);
-//		ComparingImages.diff("src/FilteredIMAGE1.JPG", "Filtered.jpg");
+	}
+
+	private static void execParallelFilter(int[] src, int[] dst, final int width, final int height, final int numberOfThreads) throws  IOException {
+		System.out.println("\nStarting parallel image filter using " + numberOfThreads + " threads.");
+
+		ForkJoinPool pool = new ForkJoinPool(numberOfThreads);
+		ParallelFJImageFilter filter;
+		long startTime = System.currentTimeMillis();
+		for (int steps = 0; steps < NRSTEPS; steps++) {
+			filter = new ParallelFJImageFilter(src, dst, width, 1, height - 1);
+			pool.invoke(filter);
+
+			// swap references
+			int[] help;
+			help = src;
+			src = dst;
+			dst = help;
+		}
+
+		long endTime = System.currentTimeMillis();
+		long tParallel = endTime - startTime;
+
+		System.out.println("Parallel image filter took " + tParallel + " milliseconds using " + numberOfThreads + " threads.");
+		timeOfParallelExecution = tParallel;
+
+		BufferedImage dstImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+		dstImage.setRGB(0, 0, width, height, dst, 0, width);
+
+		String dstName = "Filtered" + srcFileName;
+		File dstFile = new File("src/ParallelFilteredImages/" + dstName);
+		ImageIO.write(dstImage, "jpg", dstFile);
+
+		System.out.println("Output image for parallel filter: " + dstName);
 	}
 }
